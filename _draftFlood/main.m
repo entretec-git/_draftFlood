13ed1a6d0b3983f341afdaa1adc7762e60696fd4
mike brown <developer@entretec.com>
2013-02-19 23:53:23 +0800

    New Fresh GIT

---
+++ _draftFlood/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  _draftFlood
+//
+//  Created by mike on 19/02/13.
+//  Copyright (c) 2013 mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "floodAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([floodAppDelegate class]));
+    }
+}
