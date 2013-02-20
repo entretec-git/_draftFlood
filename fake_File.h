58fd320c333ac7f727beda6d416054afe30777a1
mike brown <developer@entretec.com>
2013-02-20 16:38:43 +0800

    added fake files

---
+++ fake_File.h
@@ -0,0 +1,13 @@
+//
+//  fake_File.h
+//  _draftFlood
+//
+//  Created by mike on 20/02/13.
+//  Copyright (c) 2013 mike. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface fake_File : NSObject
+
+@end
---
+++ fake_File.m
@@ -0,0 +1,13 @@
+//
+//  fake_File.m
+//  _draftFlood
+//
+//  Created by mike on 20/02/13.
+//  Copyright (c) 2013 mike. All rights reserved.
+//
+
+#import "fake_File.h"
+
+@implementation fake_File
+
+@end
